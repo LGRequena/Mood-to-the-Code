e9f7cc903b100b0c80f28c69d8a0a4f7fe0a904a
Leon.Code <gregory.requena@gmail.com>
2023-11-25 23:12:32 -0500

    CONTROL FLOW ASSIGNMENT

--- perscholas/2023-108-RTT/sei-classwork/308-JavaScript/javascript2/script3.js
+++ perscholas/2023-108-RTT/sei-classwork/308-JavaScript/javascript2/script3.js
@@ -1,48 +1,33 @@
-// console.log("Wake up, Leon");
-
-// let perSon = Leon;
-
-// if (Leon === "Wake up, perSon") {
-//     console.log("Who is this?")
-// } else {
-//     console.log("Stuck in loop matrix")
-// }
-
-// const num = 18;
-
-// if (true) {
-//     console.log("user access granted");
-//     } else {
-//     if(false)
-//     { num = (18 < 0);
-//     console.log("user access denied");
-// }
-
-/**Write a nested if...else statement.
-Declare a variable called num.
-Add an if...else statement that checks if num is positive & greater than 100.
-Add another statement that checks if num is positive but less than 100.
-Add a final statement to check if num is negative. */
-let num = 100;
-if (num > 0) {
-    console.log("num is greater");
-} else if (num < 0)  { 
-    console.log("num is positive but less than 100");
-}else {
-    num = (num < 0)
-    console.log("study");
-}
-
-//**If a learner gets 90 or higher: console.log("A")
-// If a learner get 80 or above: console.log("B")
-// If a learner get 70 or above: console.log("C")
-// If a learner get 55 or above: console.log("D")
-// Any grade lower than 55: console.log("F") 
+//**
+//     CONTROL FLOW ASSIGNMENT
 // */
-const plantArea = "0.8"
-const plantUnit = "20"
-const totalArea = "5"
-const growTh = "*2"
-const radIus = "3.1415"
 
-let plantUnit growTh
+const PI = 3.1415;
+const radius = 5;
+const areaMax = PI * 5 * 5;
+const plantArea = 0.8;
+const plantUnit = 20;
+const growthWeekly = 2;
+
+let areaMax = 78.5375;
+
+
+// expression for pruned
+if areaMax => (plantArea * plantUnit * growthWeekly);
+    console.log(plArPerWeek)
+else if areaMax => 80%;
+    console.log(plArPerWeek)
+// expression for monitored
+else if areaMax => 50%;
+console log(plArPerWeek);
+// expression for planted
+//Just got confused
+
+
+const PI = 3.1415;
+const radius = 5;
+const areaMax = PI * 5 * 5;
+const plantArea = 0.8;
+const plantUnit = 100;
+const growthWeekly = 2;
+
